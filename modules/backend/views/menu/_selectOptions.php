32b837b2818b003b7a23d0b87c5cc500edd01b19
Maksym Trofimenko <gtpoxa@gmail.com>
2014-08-03 18:13:07 +0300

    link dropdown title changed

--- modules/backend/views/menu/_selectOptions.php
+++ modules/backend/views/menu/_selectOptions.php
@@ -9,13 +9,23 @@ use yii\helpers\Html;
 
 <div class="row">
     <div class="col-md-10">
-         <?= $form->field($model, 'type')->dropDownList($items, ['class'=>'form-control']) ?>
-        
-        <?= $form->field($model, 'title') ?>
+        <?= $form->field($model, 'type')->dropDownList($items, [ 'id' => false, 'class' => 'chooseLink form-control']) ?>
+        <?= $form->field($model, 'title')->textInput(['id' => false, 'class' => 'titleLink form-control']) ?>
     </div>
 </div>
 <div class="form-group text-right">
     <?= Html::submitButton('Add', ['class' => 'btn btn-primary', 'name' => 'add-button']) ?>
 </div>
 <?php ActiveForm::end(); ?>
+
+<?php 
+
+$this->registerJs('
+        $(".chooseLink").change(function(){
+            var text = $(this).find("option:selected").text();
+            var container = $(this).parents("form").first();
+            
+            $(container).find(".titleLink").val(text);
+       });
+    ');
 
